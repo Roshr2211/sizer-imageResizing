646d156d3d947e2aac9c862b8122dcca8dce6224
Arnab Bhattacharya <arnabb689@gmail.com>
2024-04-14 14:51:43 +0530

    Dynamic sizing fld

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
+import { useState, useRef } from 'react'
 import Title from './Components/Title';
 import DropBox from './Components/DropBox';
 import Resizer from './Components/Resizer';
 
+
+/*
+{
+  name: "image.png",
+  w: 1080,
+  h: 1080,
+}
+*/
+
+
 function App() {
+  const [details, setDetails] = useState(null)
   return (
     <div className="App">
       <Title/>
       <div className='flex justify-center'>
-        <DropBox/>
-        <Resizer/>
+        <DropBox setting_img_details={setDetails}/>
+        <Resizer get_img_details={details} set_img_details={setDetails}/>
       </div>
     </div>
   );
